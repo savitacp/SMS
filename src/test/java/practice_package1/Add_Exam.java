731fcec433ae28d566cc1da452dedb3b59bd2308
Savitha <Savitha@DESKTOP-LKA9B6H>
2023-04-06 13:39:14 +0530

    script is ready reveiw it.

---
+++ src/test/java/practice_package1/Add_Exam.java
@@ -0,0 +1,9 @@
+package practice_package1;
+
+public class Add_Exam {
+
+	public static void main(String[] args) {
+		System.out.println("Test script1 executed");   
+	}
+
+}
---
+++ src/test/java/practice_package1/Add_Sub.java
@@ -0,0 +1,10 @@
+package practice_package1;
+
+public class Add_Sub {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Test script02 is executed");
+	}
+
+}
